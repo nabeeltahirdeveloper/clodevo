5cef4028aaf178f065f9c22e61cb947dd414c796
Nabeel <nabeeltahirdeveloper@gmail.com>
2024-02-01 18:20:06 +0500

    Add dropdown menu to Sidebar component

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -2,8 +2,11 @@
 import React from "react";
 import styles from "./Sidebar.module.css";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 export default function Sidebar() {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleOpen = () => setIsOpen(!isOpen);
   const location = usePathname();
   const backgroundColor = location === "/" ? "#293040" : "#0C0B0B";
   return (
@@ -17,8 +20,27 @@ export default function Sidebar() {
           </div>
           <div className={`${styles.sidebarLink} mt-8`}>
             <img src="/sidebar/icon.svg" />
-            <a href="#">Uptime</a>
+            <div className={styles.dropdown}>
+              <button className={styles.dropdownToggle}  onClick={toggleOpen}>
+                Uptime
+              </button>
+              
+            </div>
           </div>
+          {isOpen && (
+                <div className={isOpen ? styles.dropdownMenuOpen : styles.dropdownMenuClosed} style={{
+                  flexDirection: "column",
+                  display: "flex",  
+                  color: "white",
+                  marginLeft: "75px",
+                  gap: 5,
+                  marginTop: 10,
+                }}>
+                  <a className={styles.dropdownItem}  href="#">Monitors</a>
+                  <a className={styles.dropdownItem}  href="#">Heartbeats</a>
+                  <a className={styles.dropdownItem}  href="#">Status pages</a>
+                </div>
+              )}
           <div className={`${styles.sidebarLink} mt-8`}>
             <img src="/sidebar/icon.svg" />
             <a href="#">Alerting</a>
